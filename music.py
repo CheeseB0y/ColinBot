faf58a1fa67580465a4ec1ac53beaef40025aa7b
CheeseB0y <cheeseb0y1316@gmail.com>
2024-08-07 21:10:04 -0400

    Music bot function now can support multiple discord servers at once

--- music.py
+++ music.py
@@ -23,30 +23,30 @@ class Song:
             info_dict = ydl.extract_info(url, download=False)
         return info_dict
 
-class MusicQueue:
-    def __init__(self):
+class Player:
+    def __init__(self, ctx):
         self.queue = []
         self.active = False
         self.playing = None
         self.pause = False
+        self.channel = ctx.author.voice.channel
+        self.client = None
+        self.guild = ctx.guild.id
 
     def __str__(self):
-        i = 0
-        queue_str = ""
+        i = 1
+        queue_str = f"{i}: {self.playing.title} (Now Playing)"
         for song in self.queue:
             i+=1
-            queue_str = f"{queue_str}\n{i}: {song}"
+            queue_str = f"{queue_str}\n{i}: {song.title}"
         return queue_str
-
-    def get_song(self):
-        return self.queue[0]
 
     def is_empty(self):
         return len(self.queue) == 0
 
     async def enqueue(self, song: Song, ctx):
         self.queue.append(song)
-        await ctx.send("{song.title} has been added to the queue.")
+        await ctx.send(f"{song.title} has been added to the queue.")
 
     async def dequeue(self, ctx):
         if self.is_empty():
@@ -58,72 +58,67 @@ class MusicQueue:
     async def clear(self, ctx):
         self.queue = []
         self.playing = None
+        self.active = False
         await ctx.send("Queue has been emptied.")
     
     async def handle_queue(self, ctx):
         self.active = True
-        client = await get_client(ctx)
         while (not self.is_empty()):
             song = await self.dequeue(ctx)
             self.playing = song
             player = discord.FFmpegPCMAudio(song.stream, **ffmpeg_options, executable="bin/ffmpeg.exe")
             await ctx.send(f"Now playing: {song.title}")
-            client.play(player)
-            while (client.is_playing() or self.pause):
+            self.client.play(player)
+            while (self.client.is_playing() or self.pause):
                 await asyncio.sleep(1)
         self.active = False
         self.playing = None
 
-queue = MusicQueue()
-
-async def get_client(ctx):
-    if (not ctx.voice_client):
-        return await join(ctx)
-    else:
-        return ctx.guild.voice_client
+players = {}
         
 async def join(ctx):
     if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        return await channel.connect()
+        players[ctx.guild.id] = Player(ctx)
+        players[ctx.guild.id].client = await players[ctx.guild.id].channel.connect()
     else:
         await ctx.send("User is not in a voice channel.")
 
 async def leave(ctx):
+    if (players[ctx.guild.id].active):
+        await ctx.send("Cannot leave channel while queue is active. Try !stop instead.")
+        return
     if (ctx.voice_client):
-        await ctx.guild.voice_client.disconnect()
+        await players[ctx.guild.id].client.disconnect()
+        del players[ctx.guild.id]
     else:
         ctx.send("ColinBot is not currently in a voice channel.")
 
 async def play(ctx, url: str):
     song = Song(url)
-    if (ctx.author.voice):
-        await queue.enqueue(song, ctx)
-        if (not queue.active):
-            await queue.handle_queue(ctx)
+    if (ctx.guild.id not in players):
+        await join(ctx)
+    await players[ctx.guild.id].enqueue(song, ctx)
+    if (not players[ctx.guild.id].active):
+        await players[ctx.guild.id].handle_queue(ctx)
 
 async def pause(ctx):
-    client = await get_client(ctx)
-    queue.pause = True
-    client.pause()
+    players[ctx.guild.id].pause = True
+    players[ctx.guild.id].client.pause()
 
 async def resume(ctx):
-    client = await get_client(ctx)
-    queue.pause = False
-    client.resume()
+    players[ctx.guild.id].pause = False
+    await players[ctx.guild.id].client.resume()
 
 async def stop(ctx):
-    client = await get_client(ctx)
-    await queue.clear(ctx)
-    client.stop()
+    await players[ctx.guild.id].clear(ctx)
+    players[ctx.guild.id].client.stop()
     await leave(ctx)
 
 async def skip(ctx):
-    client = await get_client(ctx)
-    client.stop()
+    await players[ctx.guild.id].client.stop()
 
 async def playing(ctx):
-    await ctx.send(queue.playing)
+    await ctx.send(players[ctx.guild.id].playing)
 
 async def print_queue(ctx):
-    await ctx.send(queue)
+    await ctx.send(players[ctx.guild.id])
