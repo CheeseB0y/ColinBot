70dd18192ca945ce3bebc2e9706c795f2804ed1a
CheeseB0y <cheeseb0y1316@gmail.com>
2024-08-09 17:57:53 -0400

    updated colin function to use a private s3 bucket instead of local files

--- misc.py
+++ misc.py
@@ -1,13 +1,22 @@
 import os
 import random
 import discord
+import boto3
+from dotenv import load_dotenv
+from io import BytesIO
+
+load_dotenv()
+
+s3 = boto3.resource(service_name='s3', region_name=os.getenv('AWS_REGION'), aws_access_key_id=os.getenv('AWS_ACCESS_KEY'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+bucket = s3.Bucket(os.getenv('BUCKET_NAME'))
 
 async def colin(ctx):
-    pictures = []
-    for filepath in os.listdir("pictures/colin/"):
-        pictures.append(f"pictures/colin/{filepath}")
-    randomColin = random.randint(0, len(pictures) - 1)
-    await ctx.send(file=discord.File(pictures[randomColin]))
+    pictures = [obj.key for obj in bucket.objects.filter(Prefix='img/colin/')]
+    randomColin = random.choice(pictures)
+    file_obj = s3.Object(os.getenv('BUCKET_NAME'), randomColin)
+    file_content = file_obj.get()['Body'].read()
+    file = discord.File(BytesIO(file_content), filename=randomColin.split('/')[-1])
+    await ctx.send(file=file)
 
 async def ninja(ctx):
     await ctx.send("The phrase “it's just a game” is such a weak mindset. You are ok with what happened, losing, imperfection of a craft. When you stop getting angry after losing, you've lost twice.\n\nThere's always something to learn, and always room for improvement, never settle.")
