38aaf55dcd9401bc375531a276969dcddd9a7982
CheeseB0y <cheeseb0y1316@gmail.com>
2024-10-09 22:16:06 -0400

    fixed so that log directory will be created in the same directory as the bot

--- logging_config.py
+++ logging_config.py
@@ -13,45 +13,28 @@ class StreamToLogger(object):
         self.linebuf = ''
 
     def write(self, message):
-        if message.rstrip():  # Avoid logging empty messages
-            # Send stderr messages to ERROR level, everything else to INFO
+        if message.rstrip():
             if self.log_level == logging.ERROR and "[INFO    ]" in message:
-                self.logger.log(logging.INFO, message.rstrip())  # Fix for Discord INFO messages showing as ERROR
+                self.logger.log(logging.INFO, message.rstrip())
             else:
                 self.logger.log(self.log_level, message.rstrip())
 
     def flush(self):
-        pass  # Flush is required, but we don't need it to do anything
+        pass
 
-# Create a 'logs' folder if it doesn't exist
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 if not os.path.exists('logs'):
     os.makedirs('logs')
-
-# Create a logger
 logger = logging.getLogger('colinbot')
-
-# Create a TimedRotatingFileHandler that rotates the log file daily and keeps 30 days of logs
 log_file_handler = TimedRotatingFileHandler('logs/bot.log', when='midnight', interval=1, backupCount=30, encoding='utf-8')
 log_file_handler.setLevel(logging.INFO)
-
-# Define the format for log messages
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 log_file_handler.setFormatter(formatter)
-
-# Also log to the console
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
-
-# Add handlers to the logger
 logger.addHandler(log_file_handler)
 logger.addHandler(console_handler)
-
-# Set the logging level for the logger
 logger.setLevel(logging.INFO)
-
-# Redirect stdout (print statements) to the logger
 sys.stdout = StreamToLogger(logger, logging.INFO)
-
-# Redirect stderr (error prints) to the logger as errors
 sys.stderr = StreamToLogger(logger, logging.ERROR)
